4f392d5f043c67296a7bc3fa66fba461ded27cba
Quanggg <minhquang030120@gmail.com>
2024-09-11 01:16:43 -0500

    finish SSR FE with pud

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,12 +3,16 @@ const viewController = require('./../controllers/viewController');
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
-router.use(authController.isLoggedIn);
 
-router.get('/', viewController.getOverview);
+router.get('/', authController.isLoggedIn, viewController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
+router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
+router.get('/me', authController.protect, viewController.getAccount);
 
-router.get('/tour/:slug', viewController.getTour);
-
-router.get('/login', viewController.getLoginForm);
+router.post(
+  '/submit-user-data',
+  authController.protect,
+  viewController.updateUserData
+);
 
 module.exports = router;
